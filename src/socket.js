40d31178b17bae6706dd9eaeb3c86b1f6847f33d
bardownncelly13 <Rcof8c@gmail.com>
2025-08-16 17:39:08 -0500

    cors

--- src/socket.js
+++ src/socket.js
@@ -3,6 +3,7 @@ import { io } from "socket.io-client";
 
 const socket = io("https://studygames-backend-80244932095.us-central1.run.app", {
   transports: ["websocket"], 
+  upgrade: true,  
   withCredentials: true,    
 });
 socket.on("connect", () => {
