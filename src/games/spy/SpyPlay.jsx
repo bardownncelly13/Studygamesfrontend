745d3137e21badae4bf21190d10cefa88b5fbca3
bardownncelly13 <Rcof8c@gmail.com>
2025-08-18 00:05:34 -0500

    debug for mobile2

--- src/games/spy/SpyPlay.jsx
+++ src/games/spy/SpyPlay.jsx
@@ -218,7 +218,7 @@ useEffect(() => {
         }
       }
     };
-
+    logToScreen("right before updating players")
     socket.on("updatePlayers", handleUpdatePlayers);
     socket.on("hostIs", handleHostIs);
     socket.on("error", (err) => logToScreen("⚠️ Socket error:", err));
