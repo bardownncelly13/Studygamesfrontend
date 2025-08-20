12ce0290e716fd4f4823cb9661ac727f39fed0bf
bardownncelly13 <Rcof8c@gmail.com>
2025-08-19 22:23:33 -0500

    debug for mobile

--- src/components/deckCard.jsx
+++ src/components/deckCard.jsx
@@ -44,7 +44,7 @@ const DeckCard = ({ deck, onClick, isSelected, playDeck,onDelete,requireMotion =
         className="mt-4 bg-gradient-to-r from-purple-600 via-indigo-600 to-blue-600 text-white font-semibold px-3 py-1 rounded-full shadow-md hover:from-purple-700 hover:via-indigo-700 hover:to-blue-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition duration-300 ease-in-out"
         onClick={(e) => {
           e.stopPropagation();   
-          handlePlay()        
+          handlePlay(e)        
         }}
       >
         ▶ Play
